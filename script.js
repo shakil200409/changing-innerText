0c30d71cd00eaf5441dae79fb3524f254327e141
Shakil Ahmed <shakil200409@gmail.com>
2024-12-29 21:15:08 +0600

    Practiced to change the innerText of an html file by an event handler

---
+++ script.js
@@ -0,0 +1,7 @@
+const h1 = document.getElementById("changeMe");
+const button = document.getElementById("changeButton");
+
+button.addEventListener("click", () => {
+  //write your code here
+  h1.innerText = "Hello World";
+});
